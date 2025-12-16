59807fc6da7419c2055852317eb9755b29f62d24
Manan Jain <manansancheti02@gmail.com>
2025-12-16 19:09:23 +0530

    Add files via upload

---
+++ myfunction.py
@@ -0,0 +1,25 @@
+def Higreet(name):
+    print("Hello, " + name)
+    print("Welcome to the python class!")
+
+def dic():
+ mydict = {
+    "name": "Manan Jain",
+    "cohort": "Sam Altman",
+    "age": 20, 
+ }
+
+try:
+    y = 10
+    print(y)
+
+except NameError:
+    print("variable is not define")    
+
+else:
+    print("something else went wrong ")
+
+finally:
+    print("the try except is finished ")
+    
+
